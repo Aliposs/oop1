75c88c35ccd3c07e9c65d23ca1fd091be8e68936
Ali Elbastawesy <148086018+Aliposs@users.noreply.github.com>
2023-12-30 10:22:10 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+def critical_area(reduis):
+    area = 3.14 * (reduis) **2
+    print(f'Area of Circil That Has {reduis} Of Reduis Equals: {area}cm ')
+
+    return critical_area
+
+critical_area(12)
+
+
+
+print("aaaaaaaaaaaaaaaaaaaaaa")
